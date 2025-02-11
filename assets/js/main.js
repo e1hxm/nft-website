bd93295b107813e051e81013fc809aa8a957fddd
El'ham <elham@ro.ru>
2025-02-11 21:19:18 +0300

    Доделана часть с меню на html и css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,28 @@
 /*=============== SHOW MENU ===============*/
+let navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+let navLink = document.querySelectorAll('.nav__link')
 
+let linkAction = () =>{
+    let navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
