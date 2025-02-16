29edcedbefb01f22da541479c7783f810864d48d
El'ham <elham@ro.ru>
2025-02-16 18:49:38 +0300

    Проект готов

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,6 +25,25 @@ let linkAction = () =>{
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+let scrollHeader = () =>{
+    let header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('header-bg')
+                      : header.classList.remove('header-bg')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+let sr = ScrollReveal({
+    origin: 'botton',
+    distanse: '60px',
+    duration: 2500,
+})
+
+sr.reveal(`.home__images`, {distanse: '120px', delay: 400})
+sr.reveal(`.home__title`, {delay: 1000})
+sr.reveal(`.home__description`, {delay: 1200})
+sr.reveal(`.home__button`, {delay: 1400})
+sr.reveal(`.home__footer`, {delay: 1600})
+sr.reveal(`.home__data div`, {origin: 'right', interval: 100, delay: 1800})
+
+
